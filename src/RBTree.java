6500581a6b2911e4bbdf5b4983ab2b00587a02e0
Jamarley <chris.e.ho@protonmail.com>
2024-01-18 20:22:25 +0100

    Some final changes

--- src/RBTree.java
+++ src/RBTree.java
@@ -13,7 +13,7 @@ public class RBTree<T extends Comparable <T>> {
         public void insertNode(T key) throws IllegalArgumentException {
                 Node<T> node = root;
                 Node<T> parent = null;
-                while(node != null) {
+                while (node != null) {
                         parent = node;
                         if (key.compareTo(node.data) == 0) {
                                 throw new IllegalArgumentException("BST already contains a node with key " + key);
@@ -22,7 +22,7 @@ public class RBTree<T extends Comparable <T>> {
                 }
                 Node<T> newNode = new Node<>(key);
                 newNode.color = RED;
-                if(parent == null) {
+                if (parent == null) {
                         root = newNode;
                         newNode.color = BLACK;
                         return;
@@ -35,61 +35,64 @@ public class RBTree<T extends Comparable <T>> {
                 newNode.parent = parent;
                 // Does nothing
                 fixRedBlackPropertiesAfterInsert(newNode);
+                root.color = BLACK;
         }
-        private void fixRedBlackPropertiesAfterInsert(Node<T> node){
+        private void fixRedBlackPropertiesAfterInsert(Node<T> node) {
                 //Case 1:
-                if(!checkUncleBlack(node) && node.parent.color == RED){
+                if (!checkUncleBlack(node) && node.parent.color == RED) {
                         Node<T> grandparent = node.parent.parent;
                         grandparent.left.color = BLACK;
                         grandparent.right.color = BLACK;
                         grandparent.color = RED;
-                        if(grandparent.parent != null && grandparent.parent.color == RED)
+                        if (grandparent.parent != null && grandparent.parent.color == RED) {
                                 fixRedBlackPropertiesAfterInsert(grandparent);
-                        root.color = BLACK;
+                        }
                         return;
                 }
                 //Case 2:
-                if(checkInnerGrandson(node) && node.parent.color == RED){
+                if (checkInnerGrandson(node) && node.parent.color == RED) {
                         case2Shuffle(node);
                         return;
                 }
                 //Case 3:
-                if(!checkInnerGrandson(node) && node.parent.color == RED && checkUncleBlack(node)){
+                if (!checkInnerGrandson(node) && node.parent.color == RED && checkUncleBlack(node)){
                         Node<T> grandfather = node.parent.parent;
                         Node<T> father = node.parent;
-                        if(grandfather.left == father) {
+                        if (grandfather.left == father) {
                                 rotateRight(grandfather);
 
-                        } else if(grandfather.right == father){
+                        } else {
                                 rotateLeft(grandfather);
                         }
                         grandfather.color = RED;
                         father.color = BLACK;
-                        root.color = BLACK;
                 }
         }
 
-        private void case2Shuffle(Node<T> node){
-                if(node.parent.left == node){
+        private void case2Shuffle(Node<T> node) {
+                if (node.parent.left == node) {
                         rotateRight(node.parent);
                         rotateLeft(node.parent);
                         node.color = BLACK;
-                        if(node.left != null)
+                        if(node.left != null) {
                                 node.left.color = RED;
+                        }
                 } else {
                         rotateLeft(node.parent);
                         rotateRight(node.parent);
                         node.color = BLACK;
-                        if(node.right != null)
+                        if (node.right != null) {
                                 node.right.color = RED;
+                        }
                 }
-                root.color = BLACK;
+
         }
-        private boolean checkInnerGrandson(Node<T> grandson){
+        private boolean checkInnerGrandson(Node<T> grandson) {
                 Node<T> father = grandson.parent;
                 Node<T> grandfather = father.parent;
-                if(grandfather == null)
+                if (grandfather == null) {
                         return false;
+                }
                 //Checks if the Node is an Inner Grandson
                 return grandson == father.left && father == grandfather.right || grandson == father.right && father == grandfather.left;
         }
@@ -99,22 +102,23 @@ public class RBTree<T extends Comparable <T>> {
                 Node<T> father = grandson.parent;
                 Node<T> grandfather = father.parent;
                 //Does not exist, is not black
-                if(grandfather == null)
+                if (grandfather == null) {
                         return false;
-                if(grandfather.left == father && (grandfather.right == null || grandfather.right.color == BLACK))
+                }
+                if (grandfather.left == father && (grandfather.right == null || grandfather.right.color == BLACK)) {
                         return true;
-                if(grandfather.right == father && (grandfather.left == null || grandfather.left.color == BLACK))
-                        return true;
-                return false;
+                }
+                return grandfather.right == father && (grandfather.left == null || grandfather.left.color == BLACK);
         }
 
-        private void rotateLeft(Node<T> node){
+        private void rotateLeft(Node<T> node) {
                 Node<T> parent = node.parent;
                 Node<T> rightChild = node.right;
 
                 node.right = rightChild.left;
-                if(rightChild.left != null)
+                if (rightChild.left != null) {
                         rightChild.left.parent = node;
+                }
                 rightChild.left = node;
                 node.parent = rightChild;
 
@@ -126,36 +130,41 @@ public class RBTree<T extends Comparable <T>> {
                 Node<T> leftChild = node.left;
 
                 node.left = leftChild.right;
-                if(leftChild.right != null)
+                if (leftChild.right != null) {
                         leftChild.right.parent = node;
+                }
                 leftChild.right = node;
                 node.parent = leftChild;
 
                 replaceParentsChild(parent, node, leftChild);
         }
 
-        private void replaceParentsChild(Node<T> parent, Node<T> oldChild, Node<T> newChild){
-                if(parent == null)
-                        root = newChild;
-                else if(parent.left == oldChild)
-                        parent.left = newChild;
-                else if(parent.right == oldChild)
-                        parent.right = newChild;
-                else
+        private void replaceParentsChild(Node<T> parent, Node<T> oldChild, Node<T> newChild) throws IllegalStateException {
+                if (parent != null && parent.left != oldChild && parent.right != oldChild) {
                         throw new IllegalStateException("Node is not a child of its parent");
+                }
 
-                if(newChild != null)
+                if (parent == null) {
+                        root = newChild;
+                } else if (parent.left == oldChild) {
+                        parent.left = newChild;
+                } else {
+                        parent.right = newChild;
+                }
+
+                if (newChild != null) {
                         newChild.parent = parent;
+                }
         }
 
         public void printDot(String filename) throws IOException {
                 String str = generateDot();
-                BufferedWriter writer = new BufferedWriter(new FileWriter("./output/"+filename));
+                BufferedWriter writer = new BufferedWriter(new FileWriter("./output/" + filename));
                 writer.write(str);
                 writer.close();
         }
 
-        private String generateDot(){
+        private String generateDot() {
                 String header = """
                         digraph G {
                         \tgraph [ratio=.48];
@@ -172,21 +181,24 @@ public class RBTree<T extends Comparable <T>> {
                 queue.offer(root);
                 while(!queue.isEmpty()){
                         Node<T> current = queue.pop();
-                        String l = "";
-                        String r = "";
+                        String l;
+                        String r;
                         if(current.color == RED) {
-                                if (coloredRed.isEmpty())
+                                if (coloredRed.isEmpty()) {
                                         coloredRed.append("\t").append(current.data);
-                                else
+                                }
+                                else {
                                         coloredRed.append(", ").append(current.data);
+                                }
                         }
                         Node<T> left = current.left;
                         if(left == null) {
                                 l = "n" + numLeaves;
-                                if(nilLeaves.isEmpty())
+                                if(nilLeaves.isEmpty()) {
                                         nilLeaves.append("\t").append(l);
-                                else
+                                } else {
                                         nilLeaves.append(", ").append(l);
+                                }
                                 numLeaves++;
                         } else {
                                 l = left.data.toString();
@@ -195,10 +207,11 @@ public class RBTree<T extends Comparable <T>> {
                         Node<T> right = current.right;
                         if(right == null) {
                                 r = "n" + numLeaves;
-                                if(nilLeaves.isEmpty())
+                                if(nilLeaves.isEmpty()) {
                                         nilLeaves.append("\t").append(r);
-                                else
+                                } else {
                                         nilLeaves.append(", ").append(r);
+                                }
                                 numLeaves++;
                         } else {
                                 r = right.data.toString();
@@ -206,8 +219,9 @@ public class RBTree<T extends Comparable <T>> {
                         }
                         arrows.append("\t").append(current.data.toString()).append("->").append(l).append(", ").append(r).append(";\n");
                 }
-                if(!coloredRed.isEmpty())
+                if(!coloredRed.isEmpty()) {
                         coloredRed.append("\n\t[fillcolor=red];\n");
+                }
                 nilLeaves.append("\n\t[label=\"NIL\", shape=record, width=.4,height=.25, fontsize=16];\n");
                 return header + coloredRed + nilLeaves + arrows + "}";
         }
